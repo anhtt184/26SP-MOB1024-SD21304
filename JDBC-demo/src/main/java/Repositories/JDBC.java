eb34f15cb54c9ca20ab9c992433286e7da3a5b48
clbtrithuctre <dungbui0222@gmail.com>
2026-03-25 18:12:50 +0700

    init

--- JDBC-demo/src/main/java/Repositories/JDBC.java
+++ JDBC-demo/src/main/java/Repositories/JDBC.java
@@ -8,11 +8,13 @@ public class JDBC {
     public static Connection getSqlserverConnection() {
         if (cnn == null) {
             String databaseName = "SP26_SD21304_Demo";
+            String acc = "sa";
+            String pwd = "AnhTT184";
             try {
                 cnn = DriverManager.getConnection(
                         "jdbc:sqlserver://localhost:1433;trustServerCertificate=true;databaseName=" + databaseName + ";",
-                        "sa",
-                        "AnhTT184"
+                        acc,
+                        pwd
                 );
             } catch (SQLException e) {
                 System.out.println("Get connection error: ");
